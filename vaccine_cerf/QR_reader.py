4f13c8d74b6059bf5150886d993e926a38184e09
Minh-Hiep <minhhiepfiev57@gmail.com>
2021-07-22 16:20:53 +0200

    minor update for qr code reader

--- vaccine_cerf/QR_reader.py
+++ vaccine_cerf/QR_reader.py
@@ -11,17 +11,18 @@ from pyzbar.pyzbar import decode
 
 qr = qrtools.QR()
 if qr.decode(sys.argv[1]):
-    payload = qr.data[4:]
-    #print(payload)
+    payload = qr.data[4:] # remove HC1: prefix 
  
-    # decode Base45 (remove HC1: prefix)
+    # decode Base45
     decoded = base45.b45decode(payload)
- 
+    
     # decompress using zlib
     decompressed = zlib.decompress(decoded)
     
     # decode COSE message (no signature verification done)
     cose = CoseMessage.decode(decompressed)
+    #print(cose)
+
     
     # decode the CBOR encoded payload and print as json
     print(json.dumps(cbor2.loads(cose.payload), indent=2))
